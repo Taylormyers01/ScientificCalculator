cdf611531aba7ef6297ff6c942d7676f0e9f9a6b
taylor <taylormyers01@comcast.net>
2023-07-01 11:24:03 -0400

    Updated and included ScientificMath class. Finished basic sin,cos,tan,asin,acos and atan functions.

---
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificMath.java
@@ -0,0 +1,35 @@
+package com.zipcodewilmington.scientificcalculator;
+
+public class ScientificMath {
+
+    public static double sine(int x){
+        double output = Math.sin((double)x);
+        return output;
+    }
+
+    public static double cosine(int x){
+        double output = Math.cos((double)x);
+        return output;
+    }
+
+    public static double tan(int x){
+        double output = Math.tan((double)x);
+        return output;
+    }
+    public static double inverseSine(int x){
+        double output = Math.asin((double)x);
+        return output;
+    }
+
+    public static double inverseCosine(int x){
+        double output = Math.acos((double)x);
+        return output;
+    }
+
+    public static double inverseTan(int x){
+        double output = Math.atan((double)x);
+        return output;
+    }
+
+
+}
