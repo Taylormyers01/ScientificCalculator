82e0afab24ab643a104f11c92aeb62577aef6c6d
taylor <taylormyers01@comcast.net>
2023-07-06 10:02:27 -0400

    Got calculator display working as wanted

--- src/main/java/com/zipcodewilmington/scientificcalculator/NewConsole.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/NewConsole.java
@@ -2,6 +2,7 @@ package com.zipcodewilmington.scientificcalculator;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class NewConsole {
 
@@ -11,13 +12,13 @@ public class NewConsole {
     static double baseNum;
     static double operatorNum;
     private static String[][] operators ={ {"+", "-", "*", "/", "^", "v", "~", "_", "|", "%",
-                                        "sin", "cos", "tan", "asin", "acos", "atan", "log",
-                                        "ilog", "nlog", "inlog", "!", "SU", "SD", "Save", "View", "Clear"},
+                                        " sin ", " cos ", " tan ", " asin", " acos", " atan", " log ",
+                                        " ilog", " nlog", "inlog", "!", "  SU ", "  SD ", " Save", " View", "Clear"},
                                         {"Addition", "Subtraction", "Multiplication", "Division", "Square a number",
                                         "Square root", "Exponent", "Inverse number", "Invert Number", "Get % of num",
                                         "Sine", "Cosine", "Tangent", "Arcsine", "Arccosine", "ArcTangent", "Log",
                                         "Inverse Log", "Natural Log", "Inverse Natural Log", "Factorial", "Switch Units",
-                                        "Switch Display", "Save Current Answer", "View Saved Answer", "Clear Saved Answer"}};
+                                        "Switch Display", "Save Current Answer", "View Saved Answer", "Clear Saved Answer"}}; // len 26
 
     private static Scanner input = new Scanner(System.in);
 
@@ -79,6 +80,7 @@ public class NewConsole {
     private static String helpMenu(){
 
         StringBuilder sb = new StringBuilder();
+        System.out.println(operators[0].length);
         for (int i = 0; i < operators[0].length; i++){
             sb.append(String.format("%5s : %-20s", operators[0][i], operators[1][i]));
             if(i%2 ==0){
@@ -97,21 +99,53 @@ public class NewConsole {
         for(int i = 0; i < 4;i++){
             sb.append(sides());
         }
-        sb.append(topBorder());
-        for(int i = 0; i < 8; i++){
-            sb.append(sides());
-        }
-        sb.append(topBorder());
-
+        sb.append(bottomBorder());
+        sb.append(buttons());
         return sb.toString();
     }
 
+    private static StringBuilder buttons(){
+        StringBuilder sb = new StringBuilder();
+        String space = " ";
+        sb.append(sides());
+        for(int i = 0; i < operators[0].length - 4; i +=4){
+            sb.append("|");
+            for(int j = i; j < i + 4; j++) {
+                int spaces = (5 / operators[0][j].length() ) / 2;
+                sb.append(" [");
+                for (int x = 0; x < spaces; x++) {
+                    sb.append(space);
+                }
+                sb.append(operators[0][j]);
+                for (int y = 0; y < spaces; y++) {
+                    sb.append(space);
+                }
+                sb.append("]");
+
+
+            }
+
+            sb.append(("  |\n"));
+        }
+        sb.append(String.format("| [%5s] [%5s] [%5s] [%5s]  |\n", space, "Help", operators[0][24], operators[0][25]));
+        sb.append(bottomBorder());
+        return sb;
+    }
     private static StringBuilder topBorder(){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i<36; i++){
             sb.append("_");
         }
         sb.append("\n");
+        return sb;
+    }
+    private static StringBuilder bottomBorder(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("|");
+        for(int i = 0; i<34; i++){
+            sb.append("_");
+        }
+        sb.append("|\n");
         return sb;
     }
     private static StringBuilder sides(){
